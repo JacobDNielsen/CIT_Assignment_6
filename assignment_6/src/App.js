0f4fbf3c03b3f343e9f16b4497d07de31a45c0d8
jeff0167 <73894404+jeff0167@users.noreply.github.com>
2024-11-15 15:02:27 +0100

    Done task 5

--- assignment_6/src/App.js
+++ assignment_6/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState} from 'react';
 import Person from './Person.js';
 
-
 function App() {
   const key = process.env.REACT_APP_API_KEY; /*To access .env files, we have installed the Node package "dotenv" */
-  console.log(key);
 
   const [persons, setPersons] = useState([]);  //state is here empty array, as default
+  const [personIndex, setPersonsIndex] = useState(0);
 
   useEffect (() => 
   {
@@ -17,15 +15,33 @@ function App() {
     .then(data => setPersons(data.results))
   }
   ,[]);
-
+  
+  function ChangePersonIndex(number){
+    if(personIndex < persons.length - 1 && personIndex >= 0){
+      setPersonsIndex(personIndex + number)
+    }
+  }
+  
   return (
     <div className="App">
         {/* <h1>Task 3:</h1>
-        {persons?.map((p) => <p> {p.name}</p> )} */}
-        
-        <h1> Task 4:</h1>
-        {persons?.map((p) => <Person name ={p.name} known_for_department ={p.known_for_department} /> )}
-
+        {persons?.map((p) => <p> {p.name}</p> )} 
+        {/*         
+        <h1>Task 4:</h1> */}
+        {/* <h1>Task 5 a:</h1> */}
+        {/* {persons?.map((p, index) => <Person name ={p.name} known_for_department ={p.known_for_department} doShow={ personIndex===index ? true : false} /> )} */}
+        {/* <h1>Task 5 b:</h1>
+        {personIndex > 0 && <button onClick={e => ChangePersonIndex(-1)}>previous</button>}
+        {personIndex < persons.length -1 && <button onClick={e => ChangePersonIndex(1)}>next</button>}
+        <br></br>
+        {persons[personIndex]?.name} */}
+        <h1>Task 5 c:</h1>
+        {personIndex > 1 && <button onClick={e => ChangePersonIndex(-2)}>previous - 2</button>}
+        {personIndex > 0 && <button onClick={e => ChangePersonIndex(-1)}>previous</button>}
+        {personIndex < persons.length -1 && <button onClick={e => ChangePersonIndex(1)}>next</button>}
+        {personIndex < persons.length -2 && <button onClick={e => ChangePersonIndex(2)}>next + 2</button>}
+        <br></br>
+        {persons[personIndex]?.name}
     </div>
   );
 }
--- assignment_6/src/Person.js
+++ assignment_6/src/Person.js
@@ -1,9 +1,38 @@
-function Person({name, known_for_department}){
+import { useEffect, useState } from "react";
+
+function Person({name, known_for_department, doShow}){
+
+    const [show, setShow] = useState(doShow);
+
+    function Toggle(){
+        setShow(!show);
+    }
+
+    useEffect (()=>{
+        
+    })
+
+    function ShowPerson(){
+        if(show){
+            return(
+                <div>
+                    <p>{name}'s is known for department: {known_for_department}</p>
+                </div>
+            )
+        } 
+        return (
+            <div>
+                <p>not showing person</p>
+            </div>
+        )
+    }  
+    
     return (
-    <div>
-    <p>{name}'s is known for department: {known_for_department} </p>
-    </div>
-)
+        <div>
+            {ShowPerson()}
+            <button onClick={e => Toggle()}>{ show ? "hide": "show"}</button>
+        </div>
+    )
 }
 
 export default Person;
